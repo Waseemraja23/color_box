80c423d97aa5629081d03b2b80e0d01fb641d386
Waseemraja23 <waseem23@navgurukul.org>
2024-04-04 20:29:53 +0530

    add my files

---
+++ code.js
@@ -0,0 +1,31 @@
+let colorDisplay = document.getElementById('output');
+function red(){
+    colorDisplay.style.backgroundColor ='red';
+    
+}
+function green(){
+    colorDisplay.style.backgroundColor ='green';
+}
+function blue(){
+    colorDisplay.style.backgroundColor ='blue';
+}
+function yellow(){
+    colorDisplay.style.backgroundColor ='yellow';
+    colorDisplay.style.color='black';
+}
+function purple(){
+    colorDisplay.style.backgroundColor ='purple';
+}
+function cyan(){
+    colorDisplay.style.backgroundColor ='cyan';
+}
+function orange(){
+    colorDisplay.style.backgroundColor ='orange';
+}
+function pink(){
+    colorDisplay.style.backgroundColor ='pink';
+}
+function black(){
+    colorDisplay.style.backgroundColor ='black';
+    colorDisplay.style.color='white';
+}
